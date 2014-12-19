d81edcb8cba6ae53bfaa080150812baf9ea403ee
John Zhang <john.shimin.zhang@gmail.com>
2014-12-18 21:23:26 -0800

    add upto 1.26

--- 1-2.clj
+++ 1-2.clj
@@ -263,3 +263,142 @@ angle
                       p
                       q
                       (- count 1)))))
+
+; exercise 1.20
+
+(define (gcd a b)
+(if (= b 0)
+a
+(gcd b (remainder a b))))
+
+; with normal order operations, the entire gcd gets expanded until only primatives are remaining which means at every point the (remainder a b) must be written out as remainder(a b) while in applicative order arguments are evaluated step by step, therefore with normal order each gcd operations after the first are expanded out completely as we subsitute both a and b with (remainder a b) until all we are left with are remainder operations.
+
+; with applicative order opreations, each expression of the if then statement clauses are fully evaluated before before application the top most level operation, therefore we do not have to expand out each argument of the remainder operation completely. this significantly reduces the amount of memory and step it takes to calculate the final result when compared to the normal order version.
+
+; exercise 1.21
+
+(def smallest-divisor
+  (fn[n]
+    (find-divisor n 2)))
+
+(def find-divisor
+  (fn[n, test]
+    (cond (> (square test) n) n
+      (divides test n) test
+      :else (find-divisor n (+ test 1)))))
+
+(def divides
+  (fn[a, b]
+    (= (mod b a) 0)))
+
+(def square
+  (fn[n]
+    (* n n)))
+
+(def is-prime
+  (fn[n]
+    (= n (smallest-divisor n))))
+
+; smallest divisors for 199 199, 1999 1999, 19999 7
+
+; exercise 1.22 couldnt figure out how to calculate time and scheme implementation seems to be wonky
+(define (smallest-divisor n)
+(find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+(cond ((> (square test-divisor) n) n)
+((divides? test-divisor n) test-divisor)
+(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+(= (remainder b a) 0))
+
+(define (prime? n)
+(= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+(newline)
+(display n)
+(start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+(if (prime? n)
+(report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+(display " *** ")
+(display elapsed-time))
+
+(define (is-even n)
+  (cond ((= (remainder n 2) 0) true)
+        (else false)))
+
+(define (search-for-primes start end)
+  (cond ((< end start) false)
+        ((is-even start)
+            (search-for-primes (+ start 1) end))
+        (else (timed-prime-test start)
+                (search-for-primes (+ start 1)
+                                    end))))))
+
+; exercise 1.23 once again cannot test for time, cannot find a good time measuring function
+
+(def smallest-divisor
+  (fn[n]
+    (find-divisor n 2)))
+
+
+(def find-divisor
+  (fn[n, test]
+    (cond (> (square test) n) n
+    (divides test n) test
+    :else (find-divisor n (next-num test)))))
+
+(def divides
+  (fn[a, b]
+    (= (mod b a) 0)))
+
+(def square
+  (fn[n]
+    (* n n)))
+
+(def is-prime
+  (fn[n]
+    (= n (smallest-divisor n))))
+
+(defn next-num [n]
+  (cond (= n 2) 3
+        :else n + 2))
+
+; exercise 1.24
+(define (expmod base exp m)
+(cond ((= exp 0) 1)
+((even? exp)
+(remainder (square (expmod base (/ exp 2) m))
+m))
+(else
+  (remainder (* base (expmod base (- exp 1) m))
+  m))))
+
+(define (fermat-test n)
+(define (try-it a)
+(= (expmod a n n) a))
+(try-it (+ 1 (random (- n 1)))))
+
+
+(define (fast-prime? n times)
+(cond ((= times 0) true)
+((fermat-test n) (fast-prime? n (- times 1)))
+(else false)))
+
+(define (start-prime-test n start-time)
+(if (fast-prime? n 2)
+(report-prime (- (runtime) start-time))))
+
+; exercise 1.25
+
+; with our version of expmod we are never having to actualy deal with a ver large number, at most we are dealing with the remainder of (some number smaller than m) and m. If we were to actually use our fast exp then the remainder operation will take close to forever as doing large interger divisions is very costly
+
+; exercise 1.26
+
+; because of the multiplcation, each number has to actually be calculated twice and therefore the function grows at 2n of m while the original is at log n. (theres an additional call to expomod per 2 increases in the value of n) it also transform the memory footprint of the function into that of a tree recursion, calcuating 2 extra branches per 2 increase in n and therefore slows thing down greatly. 
